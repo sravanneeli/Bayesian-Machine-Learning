8423927e3727fa06ae8124c4f0470e76cf500819
sravanneeli <sravan.neeli267@gmail.com>
2020-12-29 12:08:52 +0530

    commit

---
+++ t_test.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy import stats
+
+N = 10
+a = np.random.randn(N) + 2
+b = np.random.randn(N)
+
+var_a = a.var(ddof=1)
+var_b = b.var(ddof=1)
+
+s = np.sqrt((var_a + var_b) / 2)
+  
+t = ((a.mean() - b.mean()) / (s * np.sqrt(2.0 / N)))
+
+df = 2*N - 2
+
+p = 1 - stats.t.cdf(t, df=df)
+
+print("t:\t", t, "p:\t", 2*p)
+
+t2, p2 = stats.ttest_ind(a, b)
+
+print("t2:\t", t2, "p2:\t", p2)
